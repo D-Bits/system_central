bd1b198a9835f05d7911ea02bf3aae512742b46c
D-Bits <lockwood.da88@gmail.com>
2025-09-11 13:50:27 -0700

    Updated compilation.

--- deploy.py
+++ deploy.py
@@ -1,9 +1,21 @@
 from subprocess import run
+import hashlib as hs
+import sys
 
 
 deploy = run(['pyinstaller', 'main.py', '-F', '-n', 'SystemCentral'])
 
+if sys.platform == 'win32':
+    with open('dist/SystemCentral.exe', 'rb') as f:
+        data = f.read()
+        exec_hash = hs.sha256(data).hexdigest()
+elif sys.platform == 'linux':
+    with open('dist/SystemCentral', 'rb') as f:
+        data = f.read()
+        exec_hash = hs.sha256(data).hexdigest() 
+
 if deploy.returncode == 0:
+    print(f'Executable SHA256: {exec_hash}')
     input('Deployment succesful. Press enter to exit.')
 else:
     input('WARNING: Deployment failed. Press enter to exit.')
